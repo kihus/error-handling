c324260c3c5885879c7cc24b595d4b845f0c959c
Bruno Nascimento <wintergamesk@gmail.com>
2025-09-06 15:23:24 -0300

    feat(account): implement deposit and withdraw

--- ErrorHandling/Entities/Account.cs
+++ ErrorHandling/Entities/Account.cs
@@ -1,12 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ErrorHandling.Entities.Exceptions;
 
 namespace ErrorHandling.Entities
 {
 	internal class Account
 	{
+		public int Number { get; set; }
+		public string? Holder { get; set; }
+		public double Balance { get; set; }
+		public double WithdrawLimit { get; set; }
+
+		public Account() { }
+
+		public Account(int number, string? holder, double balance, double withdrawLimit)
+		{
+			Number = number;
+			Holder = holder;
+			Balance = balance;
+			WithdrawLimit = withdrawLimit;
+		}
+
+		private void ValidateAmount(double amount)
+		{
+			if (amount <= 0)
+				throw new DomainException("Amount should be greater than 0");
+		}
+
+		public void Deposit(double amount)
+		{
+			ValidateAmount(amount);
+
+			Balance = amount;
+		}
+
+		public void Withdraw(double amount)
+		{
+			ValidateAmount(amount);
+
+			if(amount > WithdrawLimit)
+				throw new DomainException("Withdraw Error: The amount exceeds withdraw limit");
+
+			if(amount > Balance)
+				throw new DomainException("Withdraw Error: Not enough balance");
+
+			Balance -= amount;
+			Console.WriteLine("New Balance: " + Balance);
+		}
 	}
 }
---
+++ ErrorHandling/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErrorHandling.Entities.Exceptions
+{
+	internal class DomainException : ApplicationException
+	{
+		public DomainException(string message) : base(message) { }
+	}
+}
