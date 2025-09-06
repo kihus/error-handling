c896ae758974c3e16bf015f4863630e25c60cbd4
Bruno Nascimento <wintergamesk@gmail.com>
2025-09-06 15:35:50 -0300

    feat(Program): put system to work and error handing

--- ErrorHandling/Program.cs
+++ ErrorHandling/Program.cs
@@ -1,1 +1,36 @@
-﻿
+﻿using ErrorHandling.Entities;
+using ErrorHandling.Entities.Exceptions;
+
+try
+{
+	Console.WriteLine("Enter account data");
+
+	Console.Write("Number: ");
+	var number = int.Parse(Console.ReadLine() ?? "0");
+
+	Console.Write("Holder: ");
+	var holder = Console.ReadLine() ?? "guest";
+
+	Console.Write("Initial balance: ");
+	var initialBalance = double.Parse(Console.ReadLine() ?? "0.0");
+
+	Console.Write("Withdraw limit: ");
+	var withdrawList = double.Parse(Console.ReadLine() ?? "0");
+	Console.WriteLine();
+
+	var account = new Account(number, holder, initialBalance, withdrawList);
+
+
+	Console.Write("Enter amount for withdraw: ");
+	var amount = double.Parse(Console.ReadLine() ?? "0");
+
+	account.Withdraw(amount);
+}
+catch (DomainException e)
+{
+	Console.WriteLine(e.Message);
+}
+catch(Exception e)
+{
+	Console.WriteLine(e.Message);
+}
